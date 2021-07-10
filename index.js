c915bfaa0d8ca3bafb554a782dce40cdfdcff93d
Sebastián Ignacio Sepúlveda Barría <sebastian.sepulvedab@sansano.usm.cl>
2021-07-10 18:04:19 -0400

    index.js edited online with Bitbucket

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Funcionando en http://localhost:${port}`)
 })
+app.get('/api', (req, res) => {
+  var request = require('request');
+  var options = {
+    'method': 'GET',
+    'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/audit-report',
+    'headers': {
+    }
+  };
+  request(options, function (error, response) {
+    if (error) throw new Error(error);
+    res.send(response.body);
+  });
+})
 
-var request = require('request');
-var options = {
-  'method': 'GET',
-  'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/audit-report',
-  'headers': {
-  }
-};
-request(options, function (error, response) {
-  if (error) throw new Error(error);
-  console.log(response.body);
-});
