688582276060d62af31227483f238af2236aa1c4
Sebastián Ignacio Sepúlveda Barría <sebastian.sepulvedab@sansano.usm.cl>
2021-07-11 20:23:55 -0400

    modified:   index.js

--- index.js
+++ index.js
@@ -3,90 +3,132 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Bienvenido/a/e')
+	res.send('Bienvenido/a/e')
 })
 
 app.listen(port, () => {
-  console.log(`Funcionando en http://localhost:${port}`)
+	console.log(`Funcionando en http://localhost:${port}`)
 })
 app.get('/reportes_auditoria/:llave/:valor', (req, res) => {
-  var llave = req.params.llave;
-  var valor = req.params.valor;
-  var request = require('request');
-  var options = {
-    'method': 'GET',
-    'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/audit-report',
-    'headers': {
+  	var llave = req.params.llave;
+  	var valor = req.params.valor;
+  	var request = require('request');
+  	var options = {
+    	'method': 'GET',
+    	'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/audit-report',
+    	'headers': {
     }
-  };
-  request(options, function (error, response) {
-  if (error) throw new Error(error);
-  var _ = require("underscore");
-  var json = response.body;
-  var users = JSON.parse(json);
-  switch(llave){
-    case "status":
-      	var filtered = _.where(users["Items"], {status: valor});
-      	break;
-    case "date":
-  	    var filtered = _.where(users["Items"], {date: valor});
-      	break;
-    case "job_id":
-  	    var filtered = _.where(users["Items"], {job_id: parseInt(valor)});
-      	break;
-    case "email":
-  	    var filtered = _.where(users["Items"], {email: valor});
-      	break;
-    case "printer_id":
-  	    var filtered = _.where(users["Items"], {printer_id: parseInt(valor)});
-      	break;
-    case "all":
-    	var filtered = users["Items"];
-  }
-  res.send(filtered);
-  });
+  	};
+  	request(options, function (error, response) {
+		if (error) throw new Error(error);
+		var _ = require("underscore");
+		var json = response.body;
+		var users = JSON.parse(json);
+		switch(llave){
+			case "status":
+			var filtered = _.where(users["Items"], {status: valor});
+			break;
+			case "date":
+			var filtered = _.where(users["Items"], {date: valor});
+			break;
+			case "job_id":
+			var filtered = _.where(users["Items"], {job_id: parseInt(valor)});
+			break;
+			case "email":
+			var filtered = _.where(users["Items"], {email: valor});
+			break;
+			case "printer_id":
+			var filtered = _.where(users["Items"], {printer_id: parseInt(valor)});
+			break;
+			case "all":
+			var filtered = users["Items"];
+		}
+		res.send(filtered);
+  	});
 })
-app.get('/bire', (req, res) => {
-  var request = require('request');
-  var options = {
-    'method': 'GET',
-    'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/billing-report',
-    'headers': {
-    }
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    res.send(response.body);
-  });
+app.get('/reporte_boleta/:llave/:valor', (req, res) => {
+	var llave = req.params.llave;
+  	var valor = req.params.valor;
+  	var request = require('request');
+  	var options = {
+    	'method': 'GET',
+    	'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/billing-report',
+    	'headers': {
+    	}
+  	};
+  	request(options, function (error, response) {
+    	if (error) throw new Error(error);
+    	var _ = require("underscore");
+		var json = response.body;
+		var users = JSON.parse(json);
+		switch(llave){
+			case "year_month":
+			var filtered = _.where(users["Items"], {year_month: valor});
+			break;
+			case "account":
+			var filtered = _.where(users["Items"], {account: valor});
+			break;
+			case "prints":
+			var filtered = _.where(users["Items"], {prints: parseInt(valor)});
+			break;
+			case "all":
+			var filtered = users["Items"];
+		}
+		res.send(filtered);
+  	});
 })
-app.get('/jocare', (req, res) => {
-  var request = require('request');
-  var options = {
-    'method': 'GET',
-    'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/job-cancel-reasons',
-    'headers': {
-    }
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    res.send(response.body);
-  });
+app.get('/razon_cancelacion_trabajos/:llave/:valor', (req, res) => {
+	var llave = req.params.llave;
+  	var valor = req.params.valor;
+  	var request = require('request');
+  	var options = {
+    	'method': 'GET',
+    	'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/job-cancel-reasons',
+    	'headers': {
+		}
+	};
+	request(options, function (error, response) {
+    	if (error) throw new Error(error);
+    	var _ = require("underscore");
+		var json = response.body;
+		var users = JSON.parse(json);
+		switch(llave){
+			case "cancel_by":
+			var filtered = _.where(users["Items"], {cancel_by: valor});
+			break;
+			case "job_id":
+			var filtered = _.where(users["Items"], {job_id: parseInt(valor)});
+			break;
+			case "file_id":
+			var filtered = _.where(users["Items"], {file_id: parseInt(valor)});
+			break;
+			case "printer_name":
+			var filtered = _.where(users["Items"], {printer_name: valor});
+			break;
+			case "account_email":
+			var filtered = _.where(users["Items"], {account_email: valor});
+			break;
+			case "all":
+			var filtered = users["Items"];
+		}
+		res.send(filtered);
+  	});
 })
 app.get('/reporte_impresora/:llave/:valor', (req, res) => {
-  var llave = req.params.llave;
-  var valor = req.params.valor;
-  var request = require('request');
-  var options = {
-    'method': 'GET',
-    'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/printer-report',
-    'headers': {
-    }
-  };
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    var _ = require("underscore");
-    var json = response.body;
-    var users = JSON.parse(json);
+  	var llave = req.params.llave;
+  	var valor = req.params.valor;
+  	var request = require('request');
+  	var options = {
+    	'method': 'GET',
+    	'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/printer-report',
+    	'headers': {
+    	}
+  	};
+  	request(options, function (error, response) {
+    	if (error) throw new Error(error);
+    	var _ = require("underscore");
+    	var json = response.body;
+    	var users = JSON.parse(json);
     switch(llave){
       case "status":
           var filtered = _.where(users["Items"], {status: valor});
@@ -110,3 +152,18 @@ app.get('/reporte_impresora/:llave/:valor', (req, res) => {
   });
 })
 
+app.get('/reporte_impresora_todo/', (req, res) => {
+	var request = require('request');
+	var options = {
+	  'method': 'GET',
+	  'url': 'https://c840cfx2we.execute-api.us-east-1.amazonaws.com/dev/isw/printer-report',
+	  'headers': {
+	  }
+	};
+	request(options, function (error, response) {
+		if (error) throw new Error(error);
+		var json = response.body;
+		var users = JSON.parse(json);
+		res.send(users["Items"]);
+	});
+})
